3f490ba75e805e7c25adbe4542fd22a07039a8b9
Amber Sharpe <amberjsharpe@gmail.com>
2018-07-24 17:57:20 -0500

    moved state to board

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
     render() {
       return (
-        <button className="square" onClick={() => { this.setState({value:'X'}) }}>
-          {this.state.value}
+        <button className="square">
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null)
+      };
+    }
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square value={this.state.squares[i]} />;
     }
   
     render() {
